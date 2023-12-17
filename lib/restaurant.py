e04cf7b5e45f68f228bcfb8b4a33cb4ebb6d4019
josephmangara <josephmangara2@gmail.com>
2023-12-18 01:05:19 +0300

    association  methods

--- lib/restaurant.py
+++ lib/restaurant.py
@@ -14,6 +14,15 @@ class Restaurant:
     def customers(self):
         return list(set(review.customer for review in self.reviews_list))
 
+    def average_star_rating(self):
+        total_ratings = sum(review.rating for review in self.reviews_list)
+        total_reviews = len(self.reviews_list) 
+        if total_reviews == 0:
+            return 0
+        average = total_ratings / total_reviews
+        return average
+
+
 class Customer:
     all_customers = []
 
@@ -39,6 +48,21 @@ class Customer:
         new_review = Review(self, restaurant, rating)
         self.reviews_list.append(new_review)
         restaurant.reviews_list.append(new_review)
+
+    def num_reviews(self):
+        return len(self.reviews_list)
+    
+    @classmethod
+    def find_by_name(cls, name):
+        for customer in cls.all_customers:
+            if customer.full_name() == name:
+                return customer
+        else:
+            return None
+    
+    @classmethod
+    def find_by_given_name(cls, given_name):
+        return [customer for customer in cls.all_customers if customer.given_name() == given_name]
 
 class Review:
     all_reviews = []
@@ -70,16 +94,12 @@ bond = Customer("Bond", "J")
 cray = Customer("Cray", "W")
 matt = Customer("Matt", "D")
 
-# customer reviews 
-# review_janet = Review(janet, "Teryaki Japan", 2)
-# review_melanie = Review(melanie, "Teryaki Japan", 4.0)
-# review_bond = Review(bond, "Insta noodles", 3.5)
-# review_cray = Review(cray, "Insta noodles", 4.8)
-# review_matt = Review(matt, "Teryaki Japan", 3.2)
 review_janet = janet.add_review(teryaki, 3.8)
+review_janet = janet.add_review(instanoodles, 3)
 review_melanie = melanie.add_review(teryaki, 4.0)
 review_bond = bond.add_review(instanoodles, 3.5)
 review_cray = cray.add_review(instanoodles, 4.5)
+review_cray = cray.add_review(teryaki, 3.8)
 review_matt = matt.add_review(teryaki, 4.6)
 
 # all customers and their reviews 
@@ -105,3 +125,11 @@ for customer in teryaki.customers():
 print("\nCustomers who reviewed Instanoodles:")
 for customer in instanoodles.customers():
     print(customer.full_name())
+
+print(f"\nNumber of reviews by Janet: {janet.num_reviews()}")
+
+found_customer = Customer.find_by_name("Janet Hansel")
+print(found_customer.full_name())
+
+print(teryaki.average_star_rating())
+print(instanoodles.average_star_rating())
